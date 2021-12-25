a212ec52cb83d5b9a8eaeb21aa8ad803b8186d4d
Kunal <76587209+Pikel1997@users.noreply.github.com>
2021-12-25 16:17:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+from discord.ext import commands
+from call import add_req, ret_req
+
+
+client = commands.Bot(command_prefix='#', intents = discord.Intents.all())
+
+@client.event
+async def on_ready():
+  print("On {0.user}".format(client))
+
+@client.command(name="addItem")
+async def add_item_to_list(ctx, *msg):
+  j = " ".join(msg)
+  add_req(j)
+  await ctx.channel.send("Item added")
+
+@client.command(name="getAll")
+async def retrieve_data(ctx):
+  info = ret_req()
+  await ctx.channel.send(info)
+
+client.run(os.environ['bruh'])

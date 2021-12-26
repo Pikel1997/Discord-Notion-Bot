e2f6b9725303ee5518f97edfce71cb50d4de1c54
Kunal <76587209+Pikel1997@users.noreply.github.com>
2021-12-26 16:00:45 +0400

    Update call.py

--- call.py
+++ call.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import json
+from jsonpath_ng import parse
 
 NOTION_KEY = os.getenv('NOTION_KEY')
 NOTION_DATABASE_ID = os.environ['NOTION_DATABASE_ID']
@@ -17,17 +19,29 @@ def add_req(j):
   response = requests.post('https://api.notion.com/v1/pages', headers=headers, data=data)
 
 def ret_req():
-  
-  url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}"
+
+  url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query"
 
   payload={}
   headers = {
-    'Authorization': f'Bearer {NOTION_KEY}',
-    'Notion-Version': '2021-08-16'
+    'Notion-Version': '2021-08-16',
+    'Authorization': f"Bearer {NOTION_KEY}"
   }
 
-  response = requests.request("GET", url, headers=headers, data=payload)
+  response = requests.request("POST", url, headers=headers, data=payload)
 
-  info = response.text
-  return info
+  json_data = json.loads(response.text)
 
+  query = parse('results[*].properties.Name.title[0].plain_text')
+
+  emp_l=[]
+
+  for match in query.find(json_data):
+    emp_l.append(match.value)
+  if len(emp_l) == 0:
+    return "Table is empty"
+  else:
+    return emp_l
+
+# def trun():
+  

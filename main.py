2d7c97a8ce82fbc2cdb7f2e9a3a9e31763ac3a31
Kunal <76587209+Pikel1997@users.noreply.github.com>
2022-03-06 15:26:21 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 from discord.ext import commands
-from call import add_req, ret_req
+from call import add_req, ret_req, trun
 
 
 client = commands.Bot(command_prefix='#', intents = discord.Intents.all())
@@ -14,16 +14,16 @@ async def on_ready():
 async def add_item_to_list(ctx, *msg):
   j = " ".join(msg)
   add_req(j)
-  await ctx.channel.send("Item added")
+  await ctx.channel.send("Item added to Notion database \n https://www.notion.so/d8712d240dfa4bcbb47c71d0d834d932?v=680314a8daa848c4aebf9d2adcef4de4")
 
 @client.command(name="getAll")
 async def retrieve_data(ctx):
   emp_l = ret_req()
   await ctx.channel.send(emp_l)
 
-# @client.command(name="delAll")
-# async def truncate(ctx):
-#   trunc()
-#   await ctx.channel.send("Database Truncated")
+@client.command(name="delAll")
+async def truncate(ctx):
+  trun()
+  await ctx.channel.send("Database Truncated")
 
 client.run(os.environ['bruh'])
